d235de46ed9c1f88d0be4e24ca4542896ebe23c3
sydni wright <sydniw@udel.edu>
2023-02-14 18:46:03 -0500

    Added an image

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript, Sydni Wright
             </header>
             <h2>Second Header</h2>
-
+            <img src="..assets/panda.png" alt="Cute panda holding a tree." />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World!!!!!
