280914df5a7312c8c6b5e459960c8f02314c8ec1
sydni wright <sydniw@udel.edu>
2023-02-14 19:30:09 -0500

    Created double layout, put image in one column and list in another

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import "./App.css";
 import { Button } from "react-bootstrap";
 import img from "./assets/family.png";
+import { Container } from "react-bootstrap";
+import { Row } from "react-bootstrap";
+import { Col } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -9,12 +12,24 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript, Sydni Wright
             </header>
             <h2>My Family</h2>
-            <img src={img} alt="My siblings and I." />
-            <ol>
-                <li>My brother Justin is the oldest.</li>
-                <li>I am the middle child. </li>
-                <li>My sister Yasmine is the youngest.</li>
-            </ol>
+
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <img src={img} alt="My siblings and I." />
+                        </Col>
+                        <Col>
+                            <ol>
+                                <li>My brother Justin is the oldest.</li>
+                                <li>I am the middle child. </li>
+                                <li>My sister Yasmine is the youngest.</li>
+                            </ol>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+
             <Button onClick={() => console.log("Hello World!")}>
                 Log Hello World
             </Button>
