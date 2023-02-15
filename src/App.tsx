b6ba7b3434225d740aae3b000d980fe2af3ea2ca
sydni wright <sydniw@udel.edu>
2023-02-14 19:22:02 -0500

    CHnaged image in asset folder, imported image to make it appear, changed background color of header, created functioning button, changed list contents

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import "./App.css";
 import { Button } from "react-bootstrap";
+import img from "./assets/family.png";
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript, Sydni Wright
             </header>
-            <h2>Second Header</h2>
-            <img src="..assets/panda.png" alt="Cute panda holding a tree." />
+            <h2>My Family</h2>
+            <img src={img} alt="My siblings and I." />
             <ol>
-                <li>They are no longer endangered.</li>
-                <li>They are very fluffy.</li>
-                <li>They act like toddlers.</li>
+                <li>My brother Justin is the oldest.</li>
+                <li>I am the middle child. </li>
+                <li>My sister Yasmine is the youngest.</li>
             </ol>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World!!!!!
