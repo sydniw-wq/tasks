62027ea1d282d0399b1a871967e0421f3328f91c
sydni wright <sydniw@udel.edu>
2023-02-26 22:22:17 -0500

    Remove Dollars Function completed

--- src/arrays.ts
+++ src/arrays.ts
@@ -49,7 +49,11 @@ export function stringsToIntegers(numbers: string[]): number[] {
  */
 // Remember, you can write functions as lambdas too! They work exactly the same.
 export const removeDollars = (amounts: string[]): number[] => {
-    return [];
+    let str1: string[] = [];
+    let num1: number[] = [];
+    str1 = amounts.map((amount: string): string => amount.replace("$", ""));
+    num1 = stringsToIntegers(str1);
+    return num1;
 };
 
 /**
