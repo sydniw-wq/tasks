57a96e1fa1063e35f37441c4320af6b64854a975
sydni wright <sydniw@udel.edu>
2023-03-04 13:59:32 -0500

    GetNonEmptyQuestions function completed

--- src/nested.ts
+++ src/nested.ts
@@ -19,7 +19,13 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    const nonEmptyQuestions = questions.filter(
+        (question: Question): boolean =>
+            question.body != "" ||
+            question.expected != "" ||
+            question.options.length > 0
+    );
+    return nonEmptyQuestions;
 }
 
 /***
